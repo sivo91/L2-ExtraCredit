124139f108566494ace29558ae11e7737bc491f1
Peter <83480296+sivo91@users.noreply.github.com>
2023-10-03 17:09:31 -0700

    Update L2-ExCr.cpp

--- L2-ExCr/L2-ExCr/L2-ExCr.cpp
+++ L2-ExCr/L2-ExCr/L2-ExCr.cpp
@@ -1,54 +1,34 @@
 
-
-#include <iostream>
-#include <string>
-using namespace std;
-
-string findLongestCommonSubstring(string& A, string& B) {
-    int lenA = A.size();
-    int lenB = B.size();
-    int startA = 0;		// starting position of the longest common substring in A
-    int maxLength = 0;	// length of the longest common substring
-
-    for (int i = 0; i < lenA; i++) {
-        for (int j = 0; j < lenB; j++) {
-            int length = 0;
-            while (i + length < lenA && j + length < lenB && A[i + length] == B[j + length]) {
-                length++;
-            }
-
-            if (length > maxLength && A.substr(i, length).find(' ') == string::npos) {
-                maxLength = length;
-                startA = i;
-            }
-        }
-    }
-
-    return A.substr(startA, maxLength);
-}
-
-int main() {
-    string A = "A B C D AB C D ABC D ABCD";
-    string B = "ABCD A B C D AB C D ABC D";
-
-    string result = findLongestCommonSubstring(A, B);
-
-    // output
-    cout << "Longest Common Substring is : " << result << endl;
-
-
-    return 0;
-}
-
-/*
-
-  Longest Common Substring is : ABCD
-
-*/
-
-
-
-/******************************************************************************************************/
+//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+//* Program name    :  Longest Common Substring, L2-ExtraCredit         * 
+//*                                                                     * 
+//* Written by      : Peter Sivak                                       * 
+//*                                                                     * 
+//* Purpose         : Program is finding longest common substring of    * 
+//*                   2 strings                                         * 
+//*                                                                     * 
+//* Inputs          : No input for user, just lunch program             * 
+//*                                                                     * 
+//* Outputs         : Program display common substring if there is any  * 
+//*                                                                     * 
+//* Calls           : No internal or external calls, other than system  * 
+//*                                                                     * 
+//* Structure       : BEGIN                                             * 
+//*                       User lunch program                            *
+//                                                                      * 
+//*                   STOP                                              * 
+//*                       Program display longest common substring      * 
+//*---------------------------------------------------------------------* 
+//* Collaboration   : Got help from Prof. John Urrutia                  * 
+//*                                                                     * 
+//*                                                                     * 
+//*---------------------------------------------------------------------* 
+//* Change Log:                                                         * 
+//*                         Revision                                    * 
+//*       Date    Changed  Rel Ver Mod Purpose                          * 
+//*  10/02/23      psivak   000.000.000 Initial release of program      *  
+//*                                                                     * 
+//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 
 
@@ -56,7 +36,7 @@ int main() {
 #include <string>
 using namespace std;
 
-// Recursive function to compute length of common substring starting at A[i] and B[j]
+// recursive function
 int LCSLength(const string& A, const string& B, int i, int j, int count) {
     if (i == A.size() || j == B.size()) return count;
 
@@ -75,8 +55,8 @@ string findLongestCommonSubstring(const string& A, const string& B) {
         for (int j = 0; j < B.size(); j++) {
             int length = LCSLength(A, B, i, j, 0);
 
-            // Check if the current common substring (starting at A[i] and B[j]) 
-            // is longer than the longest found so far and doesn't contain space.
+            // check if the current common substring (starting at A[i] and B[j]) 
+            // is longer than the longest found so far
             if (length > maxLength && A.substr(i, length).find(' ') == string::npos) {
                 maxLength = length;
                 startA = i;
@@ -84,7 +64,7 @@ string findLongestCommonSubstring(const string& A, const string& B) {
         }
     }
 
-    // Extract and return the longest common substring.
+    // return longest common substring
     return A.substr(startA, maxLength);
 }
 
@@ -100,13 +80,6 @@ int main() {
 
     return 0;
 }
-
-/*
-
- Longest Common Substring is : ABCD
-
-*/
-
 
 
 
